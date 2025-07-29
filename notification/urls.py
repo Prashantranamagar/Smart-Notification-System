62a643f861e86a6872e974e4806da8025ec85144
Prashantranamagar <ranamagar.prashant@gmail.com>
2025-07-29 09:05:00 +0545

    app configuration

--- notification/urls.py
+++ notification/urls.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/v1/notification/", include("app_notification.urls")),
+    path("api/v1/notifications/", include("app_notification.urls")),
     path("api/v1/auth/", include("authentication.urls")),
+    path("api/v1/", include("post.urls")),
+
+
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/v1/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/v1/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
